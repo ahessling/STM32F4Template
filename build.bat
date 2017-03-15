0dcea91a59c62337412efa916834db9e0e1ced76
Andre Heßling <andre@andrehessling.de>
2017-03-15 20:21:39 +0100

    feat: out-of-source build

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=CMake/GNU-ARM-Toolchain.cmake" .
+mkdir build
+cd build && cmake -G "Unix Makefiles" -D "CMAKE_TOOLCHAIN_FILE=../CMake/GNU-ARM-Toolchain.cmake" ../
