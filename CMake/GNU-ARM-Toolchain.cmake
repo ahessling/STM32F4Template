ebd03ff2078ee8aea221d42469a355e1939e1074
Andre Heßling <andre@andrehessling.de>
2017-03-15 20:05:53 +0100

    fix: set CMAKE_ASM_COMPILER to -gcc because -as does not understand C defines

--- CMake/GNU-ARM-Toolchain.cmake
+++ CMake/GNU-ARM-Toolchain.cmake
@@ -6,7 +6,7 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
-set(CMAKE_ASM_COMPILER arm-none-eabi-as)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 set(CMAKE_OBJDUMP arm-none-eabi-objdump)
 
