7813ab3e0700d732bd5d649d889a553fcd78dfb9
Andre Heßling <andre@andrehessling.de>
2017-03-16 18:34:50 +0100

    feat: added some sample code to turn on the blue LED on the STM32F407 discovery board

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,22 @@
 
 extern "C" int main()
 {
+  // Switch on blue LED on STM32F407Discovery
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  GPIO_InitTypeDef gpio_out = {
+    .GPIO_Pin = GPIO_Pin_15,
+    .GPIO_Mode = GPIO_Mode_OUT,
+    .GPIO_Speed = GPIO_High_Speed,
+    .GPIO_OType = GPIO_OType_PP,
+    .GPIO_PuPd = GPIO_PuPd_NOPULL
+  };
+
+  GPIO_Init(GPIOD, &gpio_out);
+  GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_SET);
+
   for (;;);
-  
+
   return 0;
 }
